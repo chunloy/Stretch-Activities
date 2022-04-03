7c00d7bf232292219ce476895617babf919ecf06
Aaron Au <aaron.au@me.com>
2022-04-03 00:01:23 +0000

    converts strings to camel cased if whitespace exists

--- case-maker.js
+++ case-maker.js
@@ -1,15 +1,16 @@
 const camelCase = function (input) {
   let characters = input.split(''); //split string into individual characters
 
-  //look for whitespaces and convert the next character to uppercase
+  //look for whitespaces, convert the next character to uppercase then remove whitespace
   for (let i = 0; i < characters.length; i++) {
     if (characters[i] === ' ') {
       characters[i + 1] = characters[i + 1].toUpperCase(); //convert to uppercase
-      characters.splice(i,1); //remove whitespace
+      characters.splice(i, 1); //remove whitespace
     }
   }
-return characters.join('');
+  return characters.join('');
 }
+
 console.log(camelCase("this is a string"));
 console.log(camelCase("loopy lighthouse"));
 console.log(camelCase("supercalifragalisticexpialidocious"));
